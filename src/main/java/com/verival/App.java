e788994d56646594b93b9cb89de7fd721dba718e
Fernando Elger <fee.elger@gmail.com>
2020-06-16 22:23:15 -0300

    add App for maven test

---
+++ src/main/java/com/verival/App.java
@@ -0,0 +1,13 @@
+package com.verival;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
