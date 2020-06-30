115c443d72a104a9b0e39ed69a8eb0428f8f0eaa
NunesJunior <Ardel_Nunes@Dellteam.com>
2020-06-29 22:03:07 -0300

    adicionando uns detalhes cabulosos

--- src/test/java/com/verival/CustoViagemTest.java
+++ src/test/java/com/verival/CustoViagemTest.java
@@ -1,16 +1,20 @@
 package com.verival;
-
 import com.verival.casosDeUso.Politicas.CalculoCustoViagem;
-
+import com.verival.casosDeUso.Politicas.CalculoCustoViagemBasico;
+import com.verival.casosDeUso.Politicas.CustoViagem;
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.M;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class CustoViagemTest{
 
     @Test
     public void custoViagemTest(){
-        CalculoCustoViagem ccv = mock(CalculoCustoViagem.class);
-
+        CalculoCustoViagem ccv = mock(CalculoCustoViagemBasico.class);
+        when(ccv.custoViagem()).thenReturn(35.0);
+        double rEsp = 35.0;
+        CustoViagem cv = new CustoViagem(ccv);
+        double rObs = cv.custoViagem(null, null);
+        assertEquals(rEsp, rObs,0.0001);
     }
-}
+}   
